2e1c565dce18eba9138f7aa3de0f349f0b50e180
truong <truongvipper@gmail.com>
2021-11-10 03:15:36 +0700

    Prop and State

--- src/App.js
+++ src/App.js
@@ -2,18 +2,32 @@ import logo from './logo.svg';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import Menu from './components/menuComponent';
 import './App.css';
+import {DISHES} from './share/dishes';
+import { render } from 'react-dom';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Truong COCO</NavbarBrand>
-          </div>
-      </Navbar>
-      <Menu></Menu>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    {
+     this.state={
+      dishes:DISHES
+     }
+    }
+  }
+  render()
+  {
+    return (
+      <div>
+        <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Truong COCO</NavbarBrand>
+            </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}></Menu>
+      </div>
+    );
+  }
 }
 
 export default App;
